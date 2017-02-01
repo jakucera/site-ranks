195068d3a853f16b7bf67803b2f7db2505a7b7d6
Joel Kucera <joelku@microsoft.com>
2017-02-01 13:58:57 -0800

    improve perf when looking up more than one domain

--- BingRanks/BingRanks.cs
+++ BingRanks/BingRanks.cs
@@ -1,38 +1,58 @@
 ﻿using System.IO;
 using System;
+using System.Collections.Generic;
 
 namespace BingRanks
 {
-		public class Bing
-		{
-		public static int GetBingRank(string path, string domain)
-		{
-            
+    public class Bing
+    {
+        private Dictionary<string, int> rankInfo;
 
-			try
-			{
-				using (StreamReader sr = new StreamReader(path))
-				{
-					while (sr.Peek() >= 0)
-					{
-						var bingrankline = (sr.ReadLine());
+        public Bing()
+        {
+            rankInfo = new Dictionary<string, int>();
+        }
 
-						char[] delimiters = new char[] { '\t' };
-						string[] parts = bingrankline.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+        public bool LoadRankData(string path)
+        {
+            char[] delimiters = new char[] { '\t' };
+            string line;
+            string[] parts;
 
-						if (parts[0].Contains(domain))
-						{
-							return int.Parse(parts[1]);
-						}
-					}
-				}
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e.Message);
-			}
-			return -1;
-		}
-	}
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        line = sr.ReadLine();
+                        parts = line.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                        if (parts.Length == 2)
+                        {
+                            rankInfo[parts[0]] = Convert.ToInt32(parts[1]);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
 
+            return true;
+        }
+
+        public int GetBingRank(string domain)
+        {
+            int rank;
+            rankInfo.TryGetValue(domain, out rank);
+            if (rank == 0)
+            {
+                rank = Int32.MaxValue;
+            }
+            return rank;
+        }
+
+    }
 }
